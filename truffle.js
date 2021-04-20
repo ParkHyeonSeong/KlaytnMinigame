698d1dafcd4f85dfae3c477a8ea99ac3a947be3f
PURPLE <9707.hyeon@gmail.com>
2021-04-20 23:51:42 +0900

    Study Klaytn

---
+++ truffle.js
@@ -0,0 +1,16 @@
+const PrivateKeyConnector = require('connect-privkey-to-provider')
+const NETWORK_ID = '1001'
+const GASLIMIT = '20000000'
+const URL = `https://api.baobab.klaytn.net:8651`
+const PRIVATE_KEY = '0x716167aba4084d4d1b6f0877964680fd34d8679397e1c7f81c5bf180fc1af8de'
+
+module.exports = {
+  networks: {  
+    klaytn: {
+      provider: new PrivateKeyConnector(PRIVATE_KEY, URL),
+      network_id: NETWORK_ID,
+      gas: GASLIMIT,
+      gasPrice: null,
+    }
+  },
+}
