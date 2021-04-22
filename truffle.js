867cc996b0616a7b3a956fe0b99a04caea88b86c
PURPLE <9707.hyeon@gmail.com>
2021-04-22 22:56:25 +0900

    Update truffle.js

--- truffle.js
+++ truffle.js
@@ -2,7 +2,7 @@ const PrivateKeyConnector = require('connect-privkey-to-provider')
 const NETWORK_ID = '1001'
 const GASLIMIT = '20000000'
 const URL = `https://api.baobab.klaytn.net:8651`
-const PRIVATE_KEY = '0x716167aba4084d4d1b6f0877964680fd34d8679397e1c7f81c5bf180fc1af8de'
+const PRIVATE_KEY = ''
 
 module.exports = {
   networks: {  
